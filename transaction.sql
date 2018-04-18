815b19f23680fe0c940a847851b0643383222dc4
jcconnol <jcconnol4@gmail.com>
2018-04-18 13:15:07 -0500

    changed total amount variable to integer instead of string

--- transaction.sql
+++ transaction.sql
@@ -6,7 +6,7 @@ CREATE TABLE transaction (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   recordid character varying(32) NOT NULL DEFAULT(''),
   employeeid character varying(32) NOT NULL DEFAULT(''),
-  totalAmt character varying(1000) NOT NULL DEFAULT(''),
+  totalAmt int NOT NULL DEFAULT(0),
   type character varying(2) NOT NULL DEFAULT(''), --'s' for sale 'r' for return
   refid character varying(32) NOT NULL DEFAULT(''), 
   CONSTRAINT employee_pkey PRIMARY KEY (id)
